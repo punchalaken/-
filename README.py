253f3c19363d5e3344cca4a95da5398fd7168900
punchalaken <penekkakti@gmail.com>
2023-05-03 15:11:11 +0300

    added first and second function

--- README.py
+++ README.py
@@ -19,7 +19,7 @@ class Student:
 
     def rate_lec(self, lectur, course, grade):
         if isinstance(lectur, Lecturer) and course in lectur.courses_attached and course in self.courses_in_progress:
-            if 1 <= float(grade) <= 10 and grade % 1 == 0:
+            if 1 <= grade <= 10 and grade % 1 == 0:
                 if course in lectur.grades:
                     lectur.grades[course] += [grade]
                 else:
@@ -28,25 +28,25 @@ class Student:
                 print('Пожалуйста, введите целое число от 1 до 10.')
 
     def __average(self):
-        x = 0
-        y = 0
+        _ = 0
+        z = 0
         if len(self.grades) != 0:
             for i in self.grades:
                 for j in self.grades[i]:
-                    x += j
-                    y += 1
-            return x / y
+                    _ += j
+                z += len(self.grades[i])
         else:
-            return 'У студента пока нет оценок за задания'
+            return 'У студента пока нет оценок за лекции'
+        return _ / z
 
     def __lt__(self, other):
         if isinstance(other, Student) and isinstance(self, Student):
             return self.__average() < other.__average()
 
     def __str__(self):
-        ex = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.__average()}\n'\
-             f'Курсы в процессе изучения: {", ".join(self.courses_in_progress) }\n' \
-             f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        ex = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: ' \
+             f'{self.__average()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress) }\n' \
+             f'Завершенные курсы: {", ".join(self.finished_courses) if len(self.finished_courses) else "Студен пока не завершил ни одного курса."}'
         return ex
 
 
@@ -59,27 +59,27 @@ class Mentor:
 
 class Lecturer(Mentor):
 
-    all_lecterers = []
+    all_lecturers = []
 
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
-        __class__.all_lecterers.append(self)
+        __class__.all_lecturers.append(self)
 
     def __average(self):
-        x = 0
-        y = 0
+        _ = 0
+        z = 0
         if len(self.grades) != 0:
             for i in self.grades:
                 for j in self.grades[i]:
-                    x += j
-                    y += 1
-            return x / y
+                    _ += j
+                z += len(self.grades[i])
         else:
             return 'У лектора пока нет оценок за лекции'
+        return _ / z
 
     def __str__(self):
-        ex = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.__average()}'
+        ex = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {round(self.__average(), 2)}'
         return ex
 
     def __lt__(self, other):
@@ -110,13 +110,20 @@ some_student.finished_courses += ['Вводный модуль для студе
 some_student.finished_courses += ['Основы языка программирования Python']
 some_student.finished_courses += ['GIT - система контроля версий']
 
+
 other_student = Student('Артем', 'Кругляков', 'Мужчина')
 other_student.courses_in_progress += ['Python']
 other_student.courses_in_progress += ['Английский для IT-специалистов']
-other_student.courses_in_progress += ['Python']
+
+
+the_student = Student('Денис', 'Лужков', 'Мужчина')
+the_student.courses_in_progress += ['Английский для IT-специалистов']
+
+
 
 oleg_buligin = Lecturer('Олег', 'Булыгин')
 oleg_buligin.courses_attached += ['Python']
+oleg_buligin.courses_attached += ['ООП и работа с API']
 
 nikita_blinov= Lecturer('Никита', 'Блинов')
 nikita_blinov.courses_attached += ['Python']
@@ -128,16 +135,53 @@ evgeniy_slow = Reviewer('Дмитрий', 'Медленный')
 evgeniy_slow.courses_attached += ['Python']
 
 dmitriy_losev.rate_hw(some_student, 'Python', 10)
+dmitriy_losev.rate_hw(some_student, 'Python', 3)
 dmitriy_losev.rate_hw(some_student, 'Python', 10)
+dmitriy_losev.rate_hw(some_student, 'Python', 8)
+dmitriy_losev.rate_hw(some_student, 'Python', 8)
+dmitriy_losev.rate_hw(some_student, 'Python', 10)
+
+dmitriy_losev.rate_hw(the_student, 'Python', 10)
 
 dmitriy_losev.rate_hw(other_student, 'Python', 2)
 dmitriy_losev.rate_hw(other_student, 'Python', 3)
 
 some_student.rate_lec(oleg_buligin, 'Python', 10)
-some_student.rate_lec(oleg_buligin, 'Python', 9)
+some_student.rate_lec(oleg_buligin, 'Python', 8)
+some_student.rate_lec(oleg_buligin, 'ООП и работа с API', 3)
+some_student.rate_lec(oleg_buligin, 'ООП и работа с API', 2)
+some_student.rate_lec(oleg_buligin, 'ООП и работа с API', 5)
+some_student.rate_lec(oleg_buligin, 'ООП и работа с API', 7)
+some_student.rate_lec(oleg_buligin, 'ООП и работа с API', 5)
+some_student.rate_lec(oleg_buligin, 'ООП и работа с API', 10)
+
 
 some_student.rate_lec(nikita_blinov, 'Python', 9)
 some_student.rate_lec(nikita_blinov, 'Python', 6)
 
-x = 0
-for _ in Student.all_students:
+
+
+
+def all_grades_students(list_student, course_name):
+    x = 0
+    y = 0
+    for _ in list_student:
+        if course_name in _.grades:
+            for j in _.grades[course_name]:
+                x += j
+                y += 1
+    return x / y
+
+print(all_grades_students(Student.all_students, 'Python'))
+
+def all_grades_lecturers(list_lecterurs, course_name):
+    x = 0
+    y = 0
+    for _ in list_lecterurs:
+        if course_name in _.grades:
+            for j in _.grades[course_name]:
+                x += j
+                y += 1
+    return x / y
+
+print(all_grades_lecturers(Lecturer.all_lecturers, 'Python'))
